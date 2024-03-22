4dfa0b75aee2a6a8e8dadf0085d5b267a3cc54e8
ssrang1228 <163647116+ssrang1228@users.noreply.github.com>
2024-03-22 23:05:38 +0900

    Update 0321학습.py

--- 0321학습.py
+++ 0321학습.py
@@ -1,6 +1,7 @@
 word_list = ["apple", "banana", "cherry", "date", "grape", "kiwi"]
 
 print("write something")
+print("This is fruit")
 result_list = [''.join(w[::-1]) + w[-1].upper() if w[-1] not in 'aeiou' else w for word in word_list for w in word]
 print(result_list)
 
