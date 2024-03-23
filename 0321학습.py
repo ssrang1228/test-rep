eed93fb94ee5571817442bbef3cec732bfc3ade1
ssrang1228 <163647116+ssrang1228@users.noreply.github.com>
2024-03-23 19:36:34 +0900

    Update 0321학습.py

--- 0321학습.py
+++ 0321학습.py
@@ -1,6 +1,6 @@
 word_list = ["apple", "banana", "cherry", "date", "grape", "kiwi"]
 Touched by user2
-
+git remote add upstream [https://github.com/Yebin0319/test-rep]
 print("print something")
 result_list = [''.join(w[::-1]) + w[-1].upper() if w[-1] not in 'aeiou' else w for word in word_list for w in word]
 print(result_list!!!)
